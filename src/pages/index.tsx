8606c151f365624f9415924a0e352678cb5a53be
JCSnap <e0970167@u.nus.edu>
2024-03-20 06:52:39 +0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Head from "next/head";
+import Link from "next/link";
+
+import { api } from "~/utils/api";
+
+export default function Home() {
+  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <Head>
+        <title>Asset Alyze</title>
+        <meta name="description" content="Portfolio creation with AI" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-black">
+        <h1 className="text-3xl text-white">Asset Alyze</h1>
+      </main>
+    </>
+  );
+}
